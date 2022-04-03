31b367a3ae9704ef784fe1cd303d896f5b3c340e
GolubevMaxim <vfrc008@icloud.com>
2022-04-04 02:06:11 +0300

    server side teleport created

---
+++ RiptideServer/Assets/Scripts/Teleport/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Teleport
+{
+    public class Teleport : MonoBehaviour
+    {
+        [SerializeField] private ushort _targetRoomIndex;
+        [SerializeField] private GameObject _teleportOut;
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.TryGetComponent<Player.Player>(out var player))
+            {
+                Rooms.Rooms.List[_targetRoomIndex].SpawnPlayer
+                    (
+                        player.NetworkId,
+                        _teleportOut.transform.position,
+                        player.NickName
+                    );
+                
+                Destroy(player.gameObject);
+            }
+        }
+    }
+}
