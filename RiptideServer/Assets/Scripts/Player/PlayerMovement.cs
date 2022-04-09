c99b7febb1f18693d8540048cdff37cd8d63507c
GolubevMaxim <vfrc008@icloud.com>
2022-04-09 03:07:19 +0300

    velocity bugfix

--- RiptideServer/Assets/Scripts/Player/PlayerMovement.cs
+++ RiptideServer/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
@@ -7,13 +8,20 @@ namespace Player
         [SerializeField] private float speed;
         private Rigidbody2D _rigidbody2D;
 
+        private Vector2 _velocity;
+
         private void Awake()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
         }
         public void Move(Vector2 direction)
         {
-            _rigidbody2D.velocity = direction * speed;
+            _velocity = direction * speed;
+        }
+
+        private void LateUpdate()
+        {
+            _rigidbody2D.velocity = _velocity;
         }
     }
 }
